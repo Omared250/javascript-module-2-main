5724ad3b7f444dd763404835f1df727177fe9897
Omar <omared250@gmail.com>
2021-07-31 02:10:02 +0200

    first part second exercise homework

---
+++ week-1/Homework/dom-manipulation-2/js/main.js
@@ -0,0 +1,37 @@
+const blueChange = document.querySelector('#blueBtn')
+blueChange.addEventListener('click', () => {
+    const jumbBg = document.querySelector('div .jumbotron');
+    jumbBg.style.backgroundColor = "#588fbd"
+
+    const donateBg = document.querySelector('#donateBtn');
+    donateBg.style.backgroundColor = "#ffa500"
+
+    const volunterBg = document.querySelector('#volunterBtn');
+    volunterBg.style.backgroundColor = "black"
+    volunterBg.style.color = "white"
+})
+
+const orangeChange = document.querySelector('#orangeBtn')
+orangeChange.addEventListener('click', () => {
+    const jumbBg = document.querySelector('div .jumbotron');
+    jumbBg.style.backgroundColor = "#f0ad4e"
+
+    const donateBg = document.querySelector('#donateBtn');
+    donateBg.style.backgroundColor = "#5751fd"
+
+    const volunterBg = document.querySelector('#volunterBtn');
+    volunterBg.style.backgroundColor = "#31b0d5"
+    volunterBg.style.color = "white"
+})
+
+const greenChange = document.querySelector('#greenBtn')
+greenChange.addEventListener('click', () => {
+    const jumbBg = document.querySelector('div .jumbotron');
+    jumbBg.style.backgroundColor = "#87ca8a"
+
+    const donateBg = document.querySelector('#donateBtn');
+    donateBg.style.backgroundColor = "black"
+
+    const volunterBg = document.querySelector('#volunterBtn');
+    volunterBg.style.backgroundColor = "#8c9c08"
+})
