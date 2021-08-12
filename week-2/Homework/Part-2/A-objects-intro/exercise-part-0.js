488715278f7bf9b54f5785840c7d74a28bbb261d
Omar <omared250@gmail.com>
2021-08-12 23:46:08 +0200

    exercise completed

--- week-2/Homework/Part-2/A-objects-intro/exercise-part-0.js
+++ week-2/Homework/Part-2/A-objects-intro/exercise-part-0.js
@@ -7,7 +7,16 @@ Try to think of as many properties as you can!
 */
 
 let laptop = {
-    brand: "Lenovo",
-    screenSize: 13,
-    isTouchscreen: true,
+    brand: "hp",
+    screenSize: 15,
+    isTouchscreen: false,
+    storage: "1TB",
+    ram: 8,
+    processor: "Intel i7",
+    operatingSystem: "windows",
+    operatingSystemVersion: 10,
+    graphicCard: "Nvidia Geforce",
+    biosMode: "UEFI",
+    biosVersion: 3,
+    storageType: "HDD"
   };
